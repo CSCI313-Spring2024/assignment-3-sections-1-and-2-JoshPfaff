d96bbd31c0baa7c29d6a087755bb0164dac8ca4a
Josh Pfaff <josh.john.pfaff@ndsu.edu>
2025-02-27 16:41:00 -0600

    Added routing to the angular application

--- src/app/housing.service.ts
+++ src/app/housing.service.ts
@@ -1,9 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HousingLocation } from './housinglocation';
-
-@Injectable({
-  providedIn: 'root',
-})
+@Injectable({ providedIn: 'root' })
 export class HousingService {
   constructor() {}
   readonly baseUrl = 'https://angular.dev/assets/images/tutorials/common';
@@ -13,7 +10,7 @@ export class HousingService {
       name: 'Acme Fresh Start Housing',
       city: 'Chicago',
       state: 'IL',
-      photo: '${this.baseUrl}/bernard-hermant-CLKGGwIBTaY-unsplash.jpg',
+      photo: `${this.baseUrl}/bernard-hermant-CLKGGwIBTaY-unsplash.jpg`,
       availableUnits: 4,
       wifi: true,
       laundry: true,
@@ -23,7 +20,7 @@ export class HousingService {
       name: 'A113 Transitional Housing',
       city: 'Santa Monica',
       state: 'CA',
-      photo: '${this.baseUrl}/brandon-griggs-wR11KBaB86U-unsplash.jpg',
+      photo: `${this.baseUrl}/brandon-griggs-wR11KBaB86U-unsplash.jpg`,
       availableUnits: 0,
       wifi: false,
       laundry: true,
@@ -33,7 +30,7 @@ export class HousingService {
       name: 'Warm Beds Housing Support',
       city: 'Juneau',
       state: 'AK',
-      photo: '${this.baseUrl}/i-do-nothing-but-love-lAyXdl1-Wmc-unsplash.jpg',
+      photo: `${this.baseUrl}/i-do-nothing-but-love-lAyXdl1-Wmc-unsplash.jpg`,
       availableUnits: 1,
       wifi: false,
       laundry: false,
@@ -43,7 +40,7 @@ export class HousingService {
       name: 'Homesteady Housing',
       city: 'Chicago',
       state: 'IL',
-      photo: '${this.baseUrl}/ian-macdonald-W8z6aiwfi1E-unsplash.jpg',
+      photo: `${this.baseUrl}/ian-macdonald-W8z6aiwfi1E-unsplash.jpg`,
       availableUnits: 1,
       wifi: true,
       laundry: false,
@@ -53,7 +50,7 @@ export class HousingService {
       name: 'Happy Homes Group',
       city: 'Gary',
       state: 'IN',
-      photo: '${this.baseUrl}/krzysztof-hepner-978RAXoXnH4-unsplash.jpg',
+      photo: `${this.baseUrl}/krzysztof-hepner-978RAXoXnH4-unsplash.jpg`,
       availableUnits: 1,
       wifi: true,
       laundry: false,
@@ -63,7 +60,7 @@ export class HousingService {
       name: 'Hopeful Apartment Group',
       city: 'Oakland',
       state: 'CA',
-      photo: '${this.baseUrl}/r-architecture-JvQ0Q5IkeMM-unsplash.jpg',
+      photo: `${this.baseUrl}/r-architecture-JvQ0Q5IkeMM-unsplash.jpg`,
       availableUnits: 2,
       wifi: true,
       laundry: true,
@@ -73,7 +70,7 @@ export class HousingService {
       name: 'Seriously Safe Towns',
       city: 'Oakland',
       state: 'CA',
-      photo: '${this.baseUrl}/phil-hearing-IYfp2Ixe9nM-unsplash.jpg',
+      photo: `${this.baseUrl}/phil-hearing-IYfp2Ixe9nM-unsplash.jpg`,
       availableUnits: 5,
       wifi: true,
       laundry: true,
@@ -83,7 +80,7 @@ export class HousingService {
       name: 'Hopeful Housing Solutions',
       city: 'Oakland',
       state: 'CA',
-      photo: '${this.baseUrl}/r-architecture-GGupkreKwxA-unsplash.jpg',
+      photo: `${this.baseUrl}/r-architecture-GGupkreKwxA-unsplash.jpg`,
       availableUnits: 2,
       wifi: true,
       laundry: true,
@@ -93,7 +90,7 @@ export class HousingService {
       name: 'Seriously Safe Towns',
       city: 'Oakland',
       state: 'CA',
-      photo: '${this.baseUrl}/saru-robert-9rP3mxf8qWI-unsplash.jpg',
+      photo: `${this.baseUrl}/saru-robert-9rP3mxf8qWI-unsplash.jpg`,
       availableUnits: 10,
       wifi: false,
       laundry: false,
@@ -103,7 +100,7 @@ export class HousingService {
       name: 'Capital Safe Towns',
       city: 'Portland',
       state: 'OR',
-      photo: '${this.baseUrl}/webaliser-_TPTXZd9mOo-unsplash.jpg',
+      photo: `${this.baseUrl}/webaliser-_TPTXZd9mOo-unsplash.jpg`,
       availableUnits: 6,
       wifi: true,
       laundry: true,
